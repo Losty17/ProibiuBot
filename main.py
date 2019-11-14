2e66502a4ec62c391710435445b6679409619a05
Vinícius <45098519+Losty17@users.noreply.github.com>
2019-11-14 13:46:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from random import randint
+from time import sleep
 import tweepy
 
 file = open("words.txt", "r")
@@ -13,6 +14,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-n = random.randint(1,28627)
-update = "Hoje o governo proibiu " + words[n].lower()
-api.update_status(status=update)
+while True:
+    update = "Hoje o governo mandou proibir " + words[random.randint(1,28627)].lower()
+    api.update_status(status=update)
+    sleep(1800)
