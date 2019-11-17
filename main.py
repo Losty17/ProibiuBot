3559f10f2086806d3b0d6adde36773e08a8f8add
Vinícius <45098519+Losty17@users.noreply.github.com>
2019-11-16 22:03:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
 from random import randint
-from time import sleep
 import tweepy
+from time import sleep
 
+####palavras
 file = open("words.txt", "r")
 words = file.readlines()
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+####login do bot
+consumer_key = 'iC8KiB6mzDgL9xhCp5IF45wDV'
+consumer_secret = 'NmsinPXvIX4DA9dKR4Mtdg4Pr5KN3UaRlA9v4ZLLdcfYSOsRTx'
+access_token = '1194773307234639873-lUH5j86nltPvxUKH6smRhKsO0IOS58'
+access_token_secret = 'wo5TTgfusLwS3M7Ck00B7Paotju88PSR4ghUuXHxHmsb3'
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+####variaveis
+user = api.get_user('ProibiuBOT')
+contador = user.statuses_count
+TOTAL = 28627
+
+####main loop
 while True:
-    update = "Hoje o governo mandou proibir " + words[random.randint(1,28627)].lower()
+    update = "Hoje o governo mandou proibir " + words[randint(1,TOTAL)].lower()
     api.update_status(status=update)
-    sleep(1800)
+    contador += 1
+    if contador % 100 == 0:
+        porcentagem = str(round(contador / TOTAL * 100, 2))
+        update='O governo ja proibiu ' + porcentagem + '% do dicionario'
+        api.update_status(status=update)
+    sleep(3600)
